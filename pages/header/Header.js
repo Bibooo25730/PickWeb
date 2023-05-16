8de7d10e466243955a287087823e3e72dbd5b4aa
Bibooo25730 <2573018577@qq.com>
2023-05-16 22:16:26 +0800

    Header.js

--- pages/header/Header.js
+++ pages/header/Header.js
@@ -12,6 +12,13 @@ export default function Header(props) {
   const leRef = useRef(null);
   function handleClick() {
     setshow(show = !show);
+   
+      if (document.documentElement.hasAttribute('theme')) {
+        document.documentElement.removeAttribute('theme');
+      }
+      else {
+        document.documentElement.setAttribute('theme', 'dark');
+      }
     // console.log(show)
   }
   function handleLeft(e) {
@@ -23,92 +30,92 @@ export default function Header(props) {
     leRef.current.style.left = -60 + '%';
   }
   //指定位置
-  function loction(){
-     if(typeof window !== 'undefined'){
-      if(window.location.hash = '#Essaycom'){
+  function loction() {
+    if (typeof window !== 'undefined') {
+      if (window.location.hash = '#Essaycom') {
         window.location.hash = ''
       }
       window.location.hash = "#Essaycom";
-     }
-   
+    }
+
   }
 
-  function handlePage(){
-    full?full.moveSectionDown():loction();
-    
+  function handlePage() {
+    full ? full.moveSectionDown() : loction();
+
   }
 
-        return (
-       
-        <header onClick={handleContaer} className={header.header}>
-          {/* 左边 */}
+  return (
 
-          <div ref={leRef} className={header.leftdiv}>
-            <div className={header.leftContainer}>
-              <div className={header.leftTop}>
-                <a href='#'><Image className={header.img} src={ActivePic} alt="headPic" priority width={120}
-                  height={120}></Image></a>
-              </div>
-              <div className={header.leftbody}>
-                <ul className={header.leftContainers}>
-                  <li> <a href=''>工具箱</a> </li>
-                  <li><a href=''>关于</a></li>
-                  <li> <a href=''>归档</a></li>
-                  <li> <a>友链</a></li>
-                  <li><i className={'icon icon-paper-plane'}></i>  </li>
+    <header onClick={handleContaer} className={header.header}>
+      {/* 左边 */}
 
-                </ul>
-              </div>
-              <div className={header.leftfotter}>
-                <a href=''> <i className={'ic icon i-github'}></i> </a>
-
-              </div>
-            </div>
-
-
+      <div ref={leRef} className={header.leftdiv}>
+        <div className={header.leftContainer}>
+          <div className={header.leftTop}>
+            <a href='#'><Image className={header.img} src={ActivePic} alt="headPic" priority width={120}
+              height={120}></Image></a>
           </div>
-          <nav>
-            <ul className={header.nav_container}>
-              <div className={[`${header.leftButton} animate__animated animate__fadeInLeft`]}>
-                <li className={header.li} onClick={handleClick}><i className={`icon icon-brightness-moon ${show ? 'hidden' : 'show'}`}></i>
-                  <i className={`icon icon-brightness-sun  ${show ? 'show' : 'hidden'}`}></i>
-                </li>
-                <li className={header.li}> <a href=''>工具箱</a> </li>
-                <li className={header.li}><a href=''>关于</a></li>
+          <div className={header.leftbody}>
+            <ul className={header.leftContainers}>
+              <li> <a >工具箱</a> </li>
+              <li><a href='https://bibooo.top/s/about'>关于</a></li>
+              <li> <a href='https://bibooo.top/archives'>归档</a></li>
+              <li> <a href='https://bibooo.top/links'>友链</a></li>
+              <li><i className={'icon icon-paper-plane'}></i>  </li>
 
-
-              </div>
-              {/* 手机端展示 这里写的有问题 */}
-              <li className={header.Phone_SITE}>
-                <div onClick={handleLeft} className={header.lines} >
-                  <span className={header.line}></span>
-                  <span className={header.line}></span>
-                  <span className={header.line}></span>
-                </div>
-                <div className={header.title}>
-                  <h3>{process.env.NEXT_PUBLIC_NAME}</h3>
-                </div>
-                <div className={'ic  i-search'}></div>
-              </li>
-
-              <li className={header.li, header.liimg}>
-                <a href='#'><Image className={header.img} src={ActivePic} alt="headPic" priority width={120}
-                  height={120}></Image></a>
-              </li>
-              <div className={[`${header.rightButton} animate__animated animate__fadeInRight`]}>
-                <li className={header.li}> <a href=''>归档</a></li>
-                <li className={header.li}> <a>友链</a></li>
-                <li className={header.li}><i className={'icon icon-paper-plane'}></i>  </li>
-              </div>
             </ul>
-          </nav>
-          <div className={header.titleName}>
-            <h3 className={'animate__animated animate__pulse'}>{process.env.NEXT_PUBLIC_TiTlEH}</h3>
-            <p>{process.env.NEXT_PUBLIC_TiTlEP}</p>
+          </div>
+          <div className={header.leftfotter}>
+            <a href='https://github.com/Bibooo25730'> <i className={'ic icon i-github'}></i> </a>
 
           </div>
-          <div className={header.xia} title="下滑"><b onClick={handlePage}>↓</b></div>
-        </header>
-     
-        )
-      }
+        </div>
+
+
+      </div>
+      <nav>
+        <ul className={header.nav_container}>
+          <div className={[`${header.leftButton} animate__animated animate__fadeInLeft`]}>
+            <li className={header.li} onClick={handleClick}><i className={`icon icon-brightness-moon ${show ? 'hidden' : 'show'}`}></i>
+              <i className={`icon icon-brightness-sun  ${show ? 'show' : 'hidden'}`}></i>
+            </li>
+            <li className={header.li}> <a >工具箱</a> </li>
+            <li className={header.li}><a href='https://bibooo.top/s/about'>关于</a></li>
+
+
+          </div>
+          {/* 手机端展示 这里写的有问题 */}
+          <li className={header.Phone_SITE}>
+            <div onClick={handleLeft} className={header.lines} >
+              <span className={header.line}></span>
+              <span className={header.line}></span>
+              <span className={header.line}></span>
+            </div>
+            <div className={header.title}>
+              <h3>Bibooo</h3>
+            </div>
+            <div className={'ic  i-search'}></div>
+          </li>
+
+          <li className={header.li, header.liimg}>
+            <a href='#'><Image className={header.img} src={ActivePic} alt="headPic" priority width={120}
+              height={120}></Image></a>
+          </li>
+          <div className={[`${header.rightButton} animate__animated animate__fadeInRight`]}>
+            <li className={header.li}> <a href='https://bibooo.top/archives'>归档</a></li>
+            <li className={header.li}> <a href='https://bibooo.top/links'>友链</a></li>
+            <li className={header.li}><i className={'icon icon-paper-plane'}></i>  </li>
+          </div>
+        </ul>
+      </nav>
+      <div className={header.titleName}>
+        <h3 className={'animate__animated animate__pulse'}>Angel eyes</h3>
+        <p>We are told to remember the idea and not the man.</p>
+
+      </div>
+      <div className={header.xia} title="下滑"><b onClick={handlePage}>↓</b></div>
+    </header>
+
+  )
+}
