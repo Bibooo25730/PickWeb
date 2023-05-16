8db8f09cd91f6a7b101bd51eb59d18107f44a0e7
Bibooo25730 <2573018577@qq.com>
2023-05-16 22:15:51 +0800

    alova

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createAlova, useRequest } from 'alova';
+import GlobalFetch from 'alova/GlobalFetch';
+import ReactHook from 'alova/react';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const alovaInstance = createAlova({
+  baseURL: 'http://127.0.0.1:8080/api/',
+  statesHook: ReactHook,
+  requestAdapter: GlobalFetch()
+});
+export default alovaInstance;
