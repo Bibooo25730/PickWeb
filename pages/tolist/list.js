e1cfb7978658fa6f2f8eb0561f79e141611b4ffb
Bibooo25730 <Bibooo@88.com>
2023-06-25 02:42:06 +0800

    pageFull

--- pages/tolist/list.js
+++ pages/tolist/list.js
@@ -7,38 +7,65 @@ import PotPlay from '../potPlay/potplay';
 import Rtc from "../rtc/rtc"
 import Lottie from '@/components/lottie'
 import { useEffect,useState } from 'react';
+import list from "./list.module.css";
+
+
+
+const FullPage = () =>{
+    // 全屏滚动
+    // 每次切换25%
+    const [currentSlide,setCurrentSlide] = useState(0);
+    const [scrolling,setScrolling] = useState(false)
+    useEffect(()=>{
+         const handleScroll =(event) =>{
+            if(!scrolling){
+                setScrolling(true)
+                if(event.deltaY>0){
+                    console.log(currentSlide)
+                    setCurrentSlide((prevState)=>(prevState<75?prevState+25:prevState))
+                }else{
+                    setCurrentSlide((prevState)=>(prevState<=75?prevState-25:prevState))
+                }
+            }
+         }
+        window.addEventListener("wheel",handleScroll);
+         return ()=>{
+             window.removeEventListener("wheel",handleScroll)
+         }
+    },[scrolling]);
+
+    useEffect(()=>{
+        const timeout = setTimeout(()=>{
+              setScrolling(false)
+        },1000)
+        return ()=>{
+            clearTimeout(timeout)
+        }
+    },[currentSlide])
 
 
 
 
-const Fullpage = () => (
- 
+return (
+    <React.Fragment>
+        <div className={list.slide} style={{transform:`translateY(-${currentSlide}%)`}}>
+            <div className={list.section}>
+                < Header />
+            </div>
 
-  <ReactFullpage
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
+            <div className={list.section}>
+                < Color/>
 
-    render={({ state, fullpageApi }) => {
-      return (
-       
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            < Header full={fullpageApi}/>
-          </div>
-          <div className="section">
-          {/* <EssayList/> */}
-          < Color/>
-          </div>
-          <div className='section'>
-            <PotPlay/>
-          </div>
-          <div className='section'>
-            <Rtc/>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
-export default Fullpage
+            </div>
+            <div className={list.section}>
+                <PotPlay/>
+            </div>
+            <div className={list.section}>
+                <Rtc/>
+            </div>
+        </div>
+
+    </React.Fragment>
+)
+}
+export default FullPage
